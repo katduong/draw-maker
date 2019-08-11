5a1d8b4479a81f15abca7c676d7c005451588c92
kduong3 <40455384+kduong3@users.noreply.github.com>
2019-08-11 16:57:23 -0700

    first commit

---
+++ makeDraw.py
@@ -0,0 +1,400 @@
+from openpyxl import load_workbook, Workbook
+from operator import itemgetter
+import random
+
+
+def makeDraw():
+    wb = load_workbook("masterlist1.xlsx")
+    # sheet = wb['DWS']
+
+    for sheet in wb:
+        if len(sheet.title) == 3:
+            rowCount = 1
+            lfplayers = []
+            midplayers = []
+            hfplayers = []
+            flightDict = {
+                'A': ['A','AB'],
+                'B': ['AB','B','BC'],
+                'C': ['BC','C','CD'],
+                'D': ['CD','D']
+            }
+            letter = sheet.title[0]
+            flights = flightDict[letter]
+            print("*"*30)
+            print(flights)
+
+            # singles draws
+            if sheet.title[2] == 'S':
+                for row in sheet:
+                    if row[0].value == 'Last Name':
+                        continue
+                    if row[3].value == flights[0]:
+                        ln = row[0].value
+                        fn = row[1].value
+                        club = row[4].value
+                        flight = row[3].value
+                        if club == None:
+                            club='Z'
+                        hfplayers.append([fn,ln,club,flight])
+                    if row[3].value == flights[1]:
+                        ln = row[0].value
+                        fn = row[1].value
+                        club = row[4].value
+                        flight = row[3].value
+
+                        if club == None:
+                            club='Z'
+                        midplayers.append([fn,ln,club,flight])
+                    if letter == 'B' or letter == 'C':
+                        if row[3].value == flights[2]:
+                            ln = row[0].value
+                            fn = row[1].value
+                            club = row[4].value
+                            flight = row[3].value
+                            if club == None:
+                                club='Z'
+                            lfplayers.append([fn,ln,club,flight])
+
+                # midplayers = sorted(midplayers, key=itemgetter(2))
+                # lfplayers = sorted(lfplayers, key=itemgetter(2))
+                # hfplayers = sorted(hfplayers, key=itemgetter(2))
+                # print("hfplayers: ",hfplayers)
+                # print("mfplayers: ",midplayers)
+                # print("lfplayers: ",lfplayers)
+                # players = hfplayers + midplayers + lfplayers
+                # totalPlayers=players
+            # doubles draw, need to handle partners
+            else:
+                num = 0
+                for row in sheet:
+                    if row[0].value == 'Last Name':
+                        continue
+                    if row[3].value == flights[0]:
+                        ln = row[0].value
+                        fn = row[1].value
+                        club = row[5].value
+                        flight = row[3].value
+                        partner = row[4].value
+                        if club == None:
+                            club='Z'
+                        hfplayers.append([fn,ln,club,flight,partner])
+                    if row[3].value == flights[1]:
+                        ln = row[0].value
+                        fn = row[1].value
+                        club = row[5].value
+                        flight = row[3].value
+                        partner = row[4].value
+                        if club == None:
+                            club='Z'
+                        midplayers.append([fn,ln,club,flight,partner])
+                    if letter == 'B' or letter == 'C':
+                        if row[3].value == flights[2]:
+                            ln = row[0].value
+                            fn = row[1].value
+                            club = row[5].value
+                            flight = row[3].value
+                            partner = row[4].value
+
+                            if club == None:
+                                club=str(num)
+                                num += 1
+                            lfplayers.append([fn,ln,club,flight,partner])
+                # midplayers = sorted(midplayers, key=itemgetter(2))
+                # lfplayers = sorted(lfplayers, key=itemgetter(2))
+                # hfplayers = sorted(hfplayers, key=itemgetter(2))
+                #
+                # players = hfplayers + midplayers + lfplayers
+            numInClub = {}
+            for player in midplayers:
+                if player[2] in numInClub:
+                    numInClub[player[2]] += 1
+                else:
+                    numInClub[player[2]] = 1
+            for player in midplayers:
+                player.append(numInClub[player[2]])
+            midplayers = sorted(midplayers, key=itemgetter(4), reverse=True)
+            for player in lfplayers:
+                if player[2] in numInClub:
+                    numInClub[player[2]] += 1
+                else:
+                    numInClub[player[2]] = 1
+            for player in lfplayers:
+                player.append(numInClub[player[2]])
+            lfplayers = sorted(lfplayers, key=itemgetter(4), reverse=True)
+            for player in hfplayers:
+                if player[2] in numInClub:
+                    numInClub[player[2]] += 1
+                else:
+                    numInClub[player[2]] = 1
+            for player in hfplayers:
+                player.append(numInClub[player[2]])
+            hfplayers = sorted(hfplayers, key=itemgetter(4), reverse=True)
+            players = hfplayers + midplayers + lfplayers
+            totalPlayers=players
+
+
+            print("*"*20)
+            print("all players:", players)
+
+            numRows = len(hfplayers) + len(midplayers) + len(lfplayers)
+            print(numRows)
+            # if numPlayers > 32 and numPlayers <  64:
+            # number of matches that can be filled on the second round minus pullouts
+            if numRows >= 64:
+                oddIndex = [0,15,8,7,4,11,12,3,2,13,10,5,6,9,14,1]
+                evenIndex = [15,0,7,8,11,4,3,12,13,2,5,10,6,5,1,14]
+                smallerBracket = 64
+            elif numRows >= 32 and numRows < 64:
+                oddIndex = [0,7,4,3,5,2,6,1]
+                evenIndex = [7,0,3,4,2,5,1,6]
+                smallerBracket = 32
+            elif numRows >= 16 and numRows < 32:
+                oddIndex = [0,3,2,1]
+                evenIndex = [3,0,1,2]
+                smallerBracket = 16
+            elif numRows >= 8 and numRows < 16:
+                oddIndex = [0,1]
+                evenIndex = [1,0]
+                smallerBracket = 8
+            else:
+                print(sheet.title, "is too small to make a draw. You need at least 8 players to create a draw")
+                continue
+            nonPullouts = smallerBracket - (numRows - smallerBracket)
+            print(nonPullouts)
+
+
+
+            firstquadrant = [None]*int(smallerBracket/4)
+            secondquadrant = [None]*int(smallerBracket/4)
+            thirdquadrant = [None]*int(smallerBracket/4)
+            fourthquadrant = [None]*int(smallerBracket/4)
+            # topbracket:
+            # smallerbracket - (totalplayers - smallerbracket) = num of players stored in top bracket
+
+            # numQuadrants = smallerbracket/4
+            # count makes sure that players are being put into a different quadrant each time
+
+            # for the rest of the players, they will be placed in pullout brackets
+            # players of the higher flight will be matched with a player of the lower flight using a random number generator
+            # need to check that randomly selected player is not a higher flight player and also not part of the same club
+            # once all the players in the higher flight are matched with someone, the players of the lower flight can
+            # be matched with other random players in the list as long as they are from different clubs
+
+            # calculate num players from high flight
+            # subtract
+            numRemaining = len(players) - nonPullouts
+            print(numRemaining)
+            hf = 0
+            mf = 0
+            lf = 0
+            remClub = {}
+            remPlayers = []
+            hflight = []
+            mflight = []
+            lflight = []
+            for i in range(nonPullouts,len(players)):
+                # count clubs for remaining players
+                if players[i][2] in remClub:
+                    remClub[players[i][2]] += 1
+                else:
+                    remClub[players[i][2]] = 1
+                # count number of hf, mf, lf left
+                if players[i][3] == flights[0]:
+                    hf += 1
+                    hflight.append(players[i])
+                if players[i][3] == flights[1]:
+                    mf += 1
+                    mflight.append(players[i])
+                if letter == 'B' or letter == 'C':
+                    if players[i][3] == flights[2]:
+                        lf += 1
+                        lflight.append(players[i])
+            print(hflight)
+            print(mflight)
+            for player in mflight:
+                player.append(remClub[player[2]])
+            for player in lflight:
+                player.append(remClub[player[2]])
+            print("mflight with club count:",mflight)
+            mflight = sorted(mflight, key=itemgetter(4), reverse=True)
+            lflight = sorted(lflight, key=itemgetter(4), reverse=True)
+            print("*"*30)
+            print(mflight)
+            print(lflight)
+            remPlayers = hflight + mflight + lflight
+            print("remaning players: ",remPlayers)
+
+                # sum number of clubs greater than 5
+            # will be used later to offset our randint
+            sumClubs5 = 0
+            for club, numClubs in remClub.items():
+                if numClubs > 5:
+                    sumClubs5 += numClubs
+            print("*"*30)
+            print(sumClubs5)
+            # print(remPlayers)
+
+            print(hf, mf, lf)
+            pulloutmatches = []
+            haveopponents = [None]*len(remPlayers)
+            matches = 0
+            # print(players)
+            if hf == 0 and lf != 0 and mf < lf:
+                hf = mf
+            if hf<=mf or mf==0:
+                # start from front
+                print("hf < lf")
+                for i in range(len(remPlayers)):
+                    if haveopponents[i]==i:
+                        continue
+                    opponent = random.randint(hf+sumClubs5,len(remPlayers)-1)
+                    # opponent = random.randint(len(remPlayers)/2,len(remPlayers)-1)
+                    while haveopponents[opponent]==opponent or remPlayers[i][2]==remPlayers[opponent][2]:
+                        opponent = random.randint(hf+sumClubs5,len(remPlayers)-1)
+
+                    haveopponents[opponent]=opponent
+                    haveopponents[i]=i
+                    print(haveopponents)
+                    pulloutmatches.append([remPlayers[i]]+[remPlayers[opponent]])
+                    matches+=1
+            elif hf > mf and lf == 0:
+                if lf == 0 and hf != 0:
+                    lf = mf
+                for i in range(len(remPlayers)-1,-1,-1):
+                    if haveopponents[i]==i:
+                        continue
+                    opponent = random.randint(0,len(remPlayers)-mf)
+                    # opponent = random.sample(range(len(remPlayers)-mf), len(remPlayers)/2)
+                    # print(opponent)
+                    while haveopponents[opponent]==opponent or remPlayers[i][2]==remPlayers[opponent][2]:
+                        opponent = random.randint(0,len(remPlayers)-mf)
+                        # opponent = random.sample(range(len(remPlayers)-mf),len(remPlayers)/2)
+                        # print(opponent)
+
+                    haveopponents[opponent]=opponent
+                    haveopponents[i]=i
+                    print(haveopponents)
+                    pulloutmatches.append([remPlayers[i]]+[remPlayers[opponent]])
+                    matches+=1
+            # else:
+            #     for i in range(len(remPlayers)):
+            #         if haveopponents[i]==i:
+            #             continue
+            #         opponent = random.randint(hf+mf+sumClubs5,len(remPlayers)-1)
+            #         # opponent = random.randint(len(remPlayers)/2,len(remPlayers)-1)
+            #         while haveopponents[opponent]==opponent or remPlayers[i][2]==remPlayers[opponent][2]:
+            #             opponent = random.randint(hf+mf+sumClubs5,len(remPlayers)-1)
+            #
+            #         haveopponents[opponent]=opponent
+            #         haveopponents[i]=i
+            #         print(haveopponents)
+            #         pulloutmatches.append([remPlayers[i]]+[remPlayers[opponent]])
+            #         matches+=1
+
+
+            for match in pulloutmatches:
+                print(match)
+                    # start from back
+
+            count = 0
+            topbracket = 0 #players who do not have a bye
+            i = 0
+            pullouts = []
+            players = players[0:nonPullouts]
+            players = players + pulloutmatches
+            print(len(players))
+            print(players)
+            for j in range(len(players)):
+                print(i)
+                # if topbracket >= nonPullouts:
+                #     break
+                if count == 0:
+                    thirdquadrant[oddIndex[i]] = players[j]
+                    count = 1
+                elif count == 1:
+                    secondquadrant[evenIndex[i]] = players[j]
+                    count = 2
+                elif count == 2:
+                    fourthquadrant[evenIndex[i]] = players[j]
+                    count = 3
+                elif count == 3:
+                    firstquadrant[oddIndex[i]] = players[j]
+                    count = 0
+                    i += 1
+                topbracket += 1
+                print(players[j])
+            print("topbracket: ", topbracket)
+
+            # for i in range(topbracket,len(players)):
+
+            # print("*****************")
+            # print(firstquadrant)
+            # print("*****************")
+            # print(secondquadrant)
+            # print("*****************")
+            # print(thirdquadrant)
+            # print("*****************")
+            # print(fourthquadrant)
+
+            draw = firstquadrant + secondquadrant + thirdquadrant + fourthquadrant
+            print(draw)
+
+            printDraw(totalPlayers, draw, sheet.title)
+
+    #notes for tomorrow: change it so it the low flight players are playing high flight players. Suggestions: make the quadrants have a fixed length of 8 then put in players from strong to weak as far as possible. so first two entered will be on opposite sides then next will go middle then next will go in between first and middle and next will go in between middle and last etc until it is full.
+
+def printDraw(players, draw, sheetName):
+    drawTemplate = load_workbook("drawTemplate - Copy.xltx")
+    sheets = drawTemplate.sheetnames
+    print("players: ",len(players))
+    if len(players) == 8:
+        source = drawTemplate[sheets[4]]
+        sheet = drawTemplate.copy_worksheet(source)
+    elif len(players) <= 16:
+        source = drawTemplate[sheets[3]]
+        sheet = drawTemplate.copy_worksheet(source)
+    elif len(players) <= 32:
+        source = drawTemplate[sheets[2]]
+        sheet = drawTemplate.copy_worksheet(source)
+    elif len(players) <= 64:
+        source = drawTemplate[sheets[1]]
+        sheet = drawTemplate.copy_worksheet(source)
+    elif len(players) <= 128:
+        source = drawTemplate[sheets[0]]
+        sheet = drawTemplate.copy_worksheet(source)
+    print(sheet.title)
+
+    curRow = 8
+    print(sheet[3][0].value)
+    for player in draw:
+        print(player)
+        # inner match
+        if sheetName[2] == 'S':
+            if len(player) != 2:
+                sheet[curRow][2].value = player[3] + " " + player[2] + " " + player[0] + " " + player[1]
+            # pullout match
+            else:
+                sheet[curRow-3][0].value = player[0][3] + " " + player[0][2] + " " + player[0][0] + " " + player[0][1]
+                sheet[curRow+2][0].value = player[1][3] + " " + player[1][2] + " " + player[1][0] + " " + player[1][1]
+            curRow += 9
+        else:
+            if len(player) != 2:
+                sheet[curRow][2].value = player[3] + " " + player[2] + " " + player[0] + " " + player[1] + " / " + player[4]
+            # pullout match
+            else:
+                sheet[curRow-3][0].value = player[0][3] + " " + player[0][2] + " " + player[0][0] + " " + player[0][1] + " / " + player[0][4]
+                sheet[curRow+2][0].value = player[1][3] + " " + player[1][2] + " " + player[1][0] + " " + player[1][1] + " / " + player[1][4]
+            curRow += 9
+
+    sheet.title = sheetName
+    drawTemplate.save("drawTemplate - Copy.xltx")
+
+
+
+def main():
+    makeDraw()
+
+
+if __name__ == '__main__':
+    main()
---
+++ parse.py
@@ -0,0 +1,215 @@
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Font
+
+
+def parseFile():
+    wb = load_workbook('players.xlsx')
+    main = wb['Players']
+
+    flights = {
+        'AMD': 0, 'BMD': 0,'CMD': 0,'DMD': 0,'AWD': 0,
+        'BWD': 0,'CWD': 0,'DWD': 0,'AMX': 0,'BMX': 0,
+        'CMX': 0,'DMX': 0,'AMS': 0,'BMS': 0,'CMS': 0,
+        'DMS': 0,'AWS': 0,'BWS': 0,'CWS': 0,'DWS': 0,
+        'AXD': 0,'BXD': 0,'CXD': 0,'DXD': 0
+    }
+
+    header = list(main.rows)[3]
+    for cell in header:
+        cell.font = Font(bold=True)
+    # loop through every row and create a new sheet for each event
+    # player will be appended to the event's sheet
+    for row in main.rows:
+        print(row[4].value)
+        events = row[4].value
+        # skip first couple lines
+        if (events != None):
+            eventList = events.split(", ")
+            print(eventList)
+            for event in eventList:
+                if event in flights:
+                    if flights[event] == 0:
+                        flights[event] += 1
+                        newSheet = wb.create_sheet(event)
+                        newSheet.append(cell.value for cell in header)
+                        newSheet.append(cell.value for cell in row)
+                    else:
+                        wb[event].append(cell.value for cell in row)
+    # sort sheets in alphabetical order
+    wb._sheets.sort(key=lambda ws:ws.title)
+    wb.save('playerParse.xlsx')
+
+def reformatParse():
+    wb = load_workbook("playerParse.xlsx")
+    for sheet in wb:
+        if sheet.title != "Players":
+            sheet.insert_cols(5, amount=1)
+            # if sheet is singles, don't need to worry about partners
+            # just need to add reformat columns
+            # lastname, firstname, gender, event letter(s), club
+            for row in sheet:
+                playerEvents = {
+                    'MS': "",
+                    'WS': "",
+                    'MX': "",
+                    'MD': "",
+                    'WD': "",
+                    'XD': ""
+                }
+                events = row[5].value.split(", ")
+                # exclude first row
+                if events != ['Events']:
+                    for event in events:
+                        # store event letters in player events dictionary
+                        string = event[-2:]
+                        playerEvents[string] += event[0]
+                    row[4].value = playerEvents[sheet.title[1:]]
+                else:
+                    row[4].value = "Events"
+                print(sheet.title)
+            # doubles event
+            # need to create a column for doubles partners
+            # lastname, firstname, gender, event letter(s), doubles partner, club
+            if 'MD' in sheet.title or 'WD' in sheet.title or 'MX' in sheet.title or 'XD' in sheet.title:
+                # insert a column for the partner name
+                sheet.insert_cols(6, amount=1)
+                # insert a column for the partner club
+                sheet.insert_cols(7, amount=1)
+                players = {}
+                rowCount = 1
+                for row in sheet:
+                    if rowCount == 1:
+                        rowCount += 1
+                        continue
+                    # grab entry info for that player
+                    entryInfo = row[8].value
+                    currentPlayer = row[1].value + " " + row[0].value
+                    print("*"*20)
+                    print(currentPlayer)
+                    print("entry info:", entryInfo)
+                    if entryInfo != None:
+                        # gets entry info and splits into list per each event
+                        entryInfo = entryInfo.split("\n")
+                        # remove empty string from the last index of list
+                        if entryInfo[-1] == "":
+                            entryInfo.pop()
+                        print("entry info after pop()",entryInfo)
+                        # loop through each event in the player's entry entryInfo
+                        # store their partner's name in player dictionary
+                        for event in entryInfo:
+                            if sheet.title in event:
+                                print("event:",event)
+                                # partnerName contains first and last name
+                                partnerName = event[4:].split(" (")[0]
+                                partnerName = partnerName.split(" ")
+                                print("partner name:",partnerName)
+                                # stores each player as a key in the dictionary
+                                # values will be a list of lists
+                                # [[partner, names], [club, rowIndex]]
+                                players[currentPlayer] = []
+                                players[currentPlayer].append([])
+                                for name in partnerName:
+                                    players[currentPlayer][0].append(name)
+                                club = row[3].value
+                                players[currentPlayer].append([])
+                                players[currentPlayer][1].append(club)
+                                players[currentPlayer][1].append(rowCount)
+                print(players)
+                # iterate through each row again and check if player is listed in the dictionary another player's partners
+                # if a match is found, append to the first occurence of pair's dictionary, the partner's club name
+                rowCount = 1
+                for row in sheet:
+                    if rowCount == 1:
+                        rowCount += 1
+                        continue
+                    playerName = row[1].value + " " + row[0].value
+                    if playerName not in players:
+                        print(playerName, "not found in player dictionary")
+                        continue
+                    # if player's partner in dictionary doesn't have a first name or last name, skip
+                    if players[playerName][0][0] == "":
+                        continue
+                    #  check if player's partner only has a first name listed
+                    elif players[playerName][0][-1] == players[playerName][0][0]:
+                        partnerfn = players[playerName][0][0]
+                        partnerFullName = partnerfn
+                    #  otherwise, we know their partner has a first and last name
+                    else:
+                        partnerfn = players[playerName][0][0]
+                        partnerln = players[playerName][0][-1]
+                        partnerFullName = partnerfn + " " + partnerln
+
+                    # check if partner full name matches with a key in the dictionary
+                    if partnerFullName in players:
+                        playerFnInDict = players[partnerFullName][0][0]
+                        playerLnInDict = players[partnerFullName][0][-1]
+                        # if playerFnInDict == row[1].value:
+                        # store partner name in excel sheet
+                        row[5].value = partnerFullName
+                        if players[partnerFullName][1][0] != None:
+                            # store partner's club name in excel sheet
+                            row[6].value = players[partnerFullName][1][0]
+                        # elif playerLnInDict == row[0].value:
+                        #     row[5].value = partnerFullName
+                        #     if players[partnerFullName][1][0] != None:
+                        #         # store partner's club name in excel sheet
+                        #         row[6].value = players[partnerFullName][1][0]
+
+
+
+
+                    # if partnerFullName:
+                    #     print(partnerFullName)
+                    # for key in players:
+                    #     if key == partner
+                    #     playerFnInDict = player[key][0][0]
+                    #     playerLnInDict = player[key][0][-1]
+                    #     if partnerfn == playerFnInDict:
+
+
+                    rowCount += 1
+
+                            #     partner = players.get(partnerName)
+                            #     if partner != None:
+                            #         partner = partner[0].split(" ")[0]
+                            # else:
+                            #     continue
+                            # if partner != None:
+                            #     print("partner in dictionary",partner)
+                            # # if row[1].value in players.values():
+                            # if partner != None and partner == row[1].value:
+                            #     print("row[1].value: ", row[1].value, " ==", partner )
+                            #
+                            #     # insert partner's club name into dictionary
+                            #     print("partner's clubname: ", row[3].value, partner)
+                            #     players[partnerName].append(row[3].value)
+                            #     sheet[players[partnerName]][5].value = players[partnerName][1]
+                            #     # row[5].value = players[partnerName][1]
+                            #     if players[partnerName][2]:
+                            #         # row[6].value = players[partnerName][2]
+                            #         sheet[players[partnerName][0]][6].value = players[partnerName][2]
+                            #     print("player key:",players[partnerName][1])
+                            #     # sheet.delete_rows(rowCount,1)
+                            # else:
+                            #     # store the first occurence of a pair in the dictionary
+                            #     players[row[1].value + " " + row[0].value] = []
+                            #     # players[row[1].value + " "  + row[0].value].append(rowCount)
+                            #     players[row[1].value + " " + row[0].value].append(partnerName)
+                            #
+                    rowCount += 1
+                    # index = entryInfo.find(sheet.title)
+                    # partnerIndex = index += 4
+                    # print(partnerIndex)
+                    # partnerName = entryInfo[partnerIndex:]
+
+
+    wb.save("playerList.xlsx")
+
+
+def main():
+    # parseFile()
+    reformatParse()
+
+
+if __name__ == '__main__':
+    main()
